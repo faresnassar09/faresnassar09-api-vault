150188d2fab8f0c531299c1b24308b6f47d71b37
Fares  Ahmed <fares.ahmed0000099999@gmail.com>
2026-02-10 00:34:48 +0200

    update composer.json

---
+++ src/ApiVaultServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace FaresNassar\ApiVault;
+
+use Illuminate\Support\ServiceProvider;
+
+class ApiVaultServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+    }
+
+    public function boot()
+    {
+    }
+}
